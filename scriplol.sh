f2d7db428c22fc65f5cab507c46e44d99a0be88d
LPD Hiro <96115049+RoHiro@users.noreply.github.com>
2022-02-10 08:54:14 +0100

    Update scriplol.sh
    Just auto install without need to click "Y" everytime

--- scriplol.sh
+++ scriplol.sh
@@ -6,25 +6,25 @@ else
         #Actualizando el sistema
         echo "Actualizando Ubuntu y todos sus paquetes"
         sleep 5s
-        sudo apt upgrade
-        sudo apt update
+        sudo apt upgrade -y
+        sudo apt update -y
         sleep 5s
         #Añadiendo todos los repositorios
         echo "Añadiendo los repositorios necesarios"
         sleep 5s
-        sudo add-apt-repository http://archive.ubuntu.com/ubuntu
+        sudo add-apt-repository http://archive.ubuntu.com/ubuntu -y
         sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-        sudo apt update
+        sudo apt update -y
         sleep 5s
         #Descargando todo
         echo "Descargando todo, Ya casi estamos!"
         sleep 5s
-        sudo apt install Git
-        sudo apt install Nano
-        sudo apt install Vifm
-        sudo apt install xorg-xrandr
-        sudo apt-get install curl
+        sudo apt install Git -y
+        sudo apt install Nano -y
+        sudo apt install Vifm -y
+        sudo apt install xorg-xrandr -y
+        sudo apt-get install curl -y
         curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-        sudo apt-get install speedtest
+        sudo apt-get install speedtest -y
     fi
 exit 1
