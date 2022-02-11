bdb77b37171754a3b211e9e48afa686b5d116a3d
LPD Hiro <96115049+RoHiro@users.noreply.github.com>
2022-02-11 11:10:10 +0100

    Update magia.sh

--- magia.sh
+++ magia.sh
@@ -4,20 +4,26 @@ if [[ $EUID -ne 0 ]]; then
    	exit 1
 else
         #Actualizando el sistema
+        echo " /-----------------------------\ "
         echo "Actualizando Ubuntu y todos sus paquetes"
+        echo " \-----------------------------/ "
         sleep 5s
         sudo apt upgrade -y
         sudo apt update -y
         sleep 5s
         #Añadiendo todos los repositorios
+        echo " /-----------------------------\ "
         echo "Añadiendo los repositorios necesarios"
+        echo " \-----------------------------/ "
         sleep 5s
         sudo add-apt-repository http://archive.ubuntu.com/ubuntu -y
         sudo add-apt-repository ppa:nilarimogard/webupd8 -y
         sudo apt update -y
         sleep 5s
         #Descargando todo
+        echo " /-----------------------------\ "
         echo "Descargando todo, Ya casi estamos!"
+        echo " \-----------------------------/ "
         sleep 5s
         sudo apt install git -y
         sudo apt install nano -y
@@ -26,5 +32,10 @@ else
         sudo apt-get install curl -y
         curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
         sudo apt-get install speedtest -y
+        echo " /-----------------------------\ "
+        echo " /-----------------------------\ "
+        echo " ----------TODO LISTO!---------- "
+        echo " \-----------------------------/ "
+        echo " \-----------------------------/ "
     fi
 exit 1
