40eec626d37fc36850010f7fde6df89898c53a0f
LPD Hiro <96115049+RoHiro@users.noreply.github.com>
2022-07-15 23:02:33 +0200

    Update magia.sh

--- magia.sh
+++ magia.sh
@@ -40,13 +40,6 @@ else
         /configure
         make
         cd ~
-        wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-        chmod +x /usr/local/bin/oh-my-posh
-        mkdir ~/.poshthemes
-        wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-        unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-        chmod u+rw ~/.poshthemes/*.json
-        rm ~/.poshthemes/themes.zip
         echo " /-----------------------------\ "
         echo " /-----------------------------\ "
         echo " ----------TODO LISTO!---------- "
